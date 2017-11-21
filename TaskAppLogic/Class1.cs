eb8a7a8054a935386d3ce702eec8dae37932ca2f
Ryan Petrie <ryanpetrie@gmail.com>
2017-11-21 09:21:49 -0700

    Initial commit.

---
+++ TaskAppLogic/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskAppLogic
+{
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public interface IUser
+    {
+        string UserName { get; }
+    }
+
+    public interface ITask
+    {
+        string Title { get; set; }
+        string Description { get; set; }
+        string AssignedTo { get; set; }
+        DateTime Due { get; set; }
+        Priority Priority { get; set; }
+    }
+
+    public interface IUserDatabase
+    {
+        IUser GetUser(string username, string password);
+    }
+
+    public interface ITaskDatabase
+    {
+        IEnumerable<ITask> GetTasks(string userName);
+        ITask NewTask();
+        void SaveTask(ITask task);
+    }
+}
---
+++ TaskAppTests/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TaskAppLogic;
+
+namespace TaskAppTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void UserTest()
+        {
+            IUserDatabase database = new MyUserDatabase();
+
+            IUser george = database.GetUser("george", "monkey!");
+            Assert.IsNotNull(george);
+            Assert.AreEqual("george", george.UserName);
+        }
+
+        [Test]
+        public void TaskTest()
+        {
+            ITaskDatabase database = new MyTaskDatabase();
+
+            ITask task = database.NewTask();
+            task.AssignedTo = "george";
+            task.Title = "George's task";
+            database.SaveTask(task);
+
+            var tasks = database.GetTasks("george");
+            Assert.Greater(tasks.Count(), 0);
+            Assert.AreEqual("george", tasks.First().AssignedTo);
+        }
+    }
+}
