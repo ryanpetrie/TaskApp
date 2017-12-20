fd430815c31d4e0955b03fdd09ecdd50d4cc8a04
Ryan Petrie <ryanpetrie@gmail.com>
2017-12-20 07:21:40 -0700

    Added group interfaces.

--- TaskAppLogic/Interfaces.cs
+++ TaskAppLogic/Interfaces.cs
@@ -13,6 +13,12 @@ namespace TaskAppLogic
         High
     }
 
+    public enum GroupRole
+    {
+        Member,
+        Owner
+    }
+
     public interface IUser
     {
         string UserName { get; }
@@ -33,6 +39,18 @@ namespace TaskAppLogic
         bool Completed { get; set; }
     }
 
+    public interface IGroup
+    {
+        string Name { get; set; }
+
+        IEnumerable<IUser> GetUsers();
+        bool IsUserInGroup(IUser user);
+        void AddUser(ILoggedInUser loggedInUser, IUser userToAdd);
+        void RemoveUser(ILoggedInUser loggedInUser, IUser userToRemove);
+        GroupRole GetRoleForUser(IUser user);
+        void SetRoleForUser(ILoggedInUser loggedInUser, IUser user, GroupRole role);
+    }
+
     public interface IUserDatabase
     {
         ILoggedInUser Login(string username, string password);
@@ -44,5 +62,12 @@ namespace TaskAppLogic
         IEnumerable<ITask> GetTasks(IUser user);
         ITask NewTask();
         void SaveTask(ITask task);
+    }
+
+    public interface IGroupDatabase
+    {
+        IEnumerable<IGroup> GetGroups(IUser user);
+        IGroup NewGroup();
+        void SaveGroup(IGroup Group);
     }
 }
